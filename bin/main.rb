914497fa1d004d4422d9e94d6c2eb87df116e377
guillainbisimwa <guillainbisimwa@hotmail.fr>
2020-10-21 13:03:46 +0200

    Make a basic twitter bot

--- bin/main.rb
+++ bin/main.rb
@@ -1,14 +1,17 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
-require './config'
+require 'twitter'
+require_relative '../lib/config.rb'
 
-key_words = ['I am a bugs', 'Bugs every where']
+key_words = ['I am a Test message', 'Test message every where']
 
 2.times do
-  client.update(key_words.sample)
-  sleep(2.minutes)
+  # client.update(key_words.sample)
+  puts 'Test'
+  # sleep(2.minutes)
 end
 
-client.search('bug').take(3).each do |tweet|
-  client.update("@#{tweet.user.screen_name} hey how are you bug?")
-end
+# client.search('bug').take(3).each do |tweet|
+#   client.update("@#{tweet.user.screen_name} hey how are you bug?")
+# end
