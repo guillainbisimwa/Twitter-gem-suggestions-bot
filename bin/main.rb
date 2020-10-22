2d72fb0af0c6e6e79bd5d7326b654d29d025e03d
guillainbisimwa <guillainbisimwa@hotmail.fr>
2020-10-23 00:23:31 +0200

    Make a simple twitter bot

--- bin/main.rb
+++ bin/main.rb
@@ -1,17 +1,37 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'dotenv/load'
 require 'twitter'
-require_relative '../lib/config.rb'
 
-key_words = ['I am a Test message', 'Test message every where']
-
-2.times do
-  # client.update(key_words.sample)
-  puts 'Test'
-  # sleep(2.minutes)
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key = ENV['CONSUMER_KEY']
+  config.consumer_secret = ENV['CONSUMER_SECRET']
+  config.access_token = ENV['ACCESS_TOKEN']
+  config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
 end
 
-# client.search('bug').take(3).each do |tweet|
-#   client.update("@#{tweet.user.screen_name} hey how are you bug?")
-# end
+stream = Twitter::Streaming::Client.new do |config|
+  config.consumer_key = ENV['CONSUMER_KEY']
+  config.consumer_secret = ENV['CONSUMER_SECRET']
+  config.access_token = ENV['ACCESS_TOKEN']
+  config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+end
+
+client.search('#hirerubybot').take(1).each do |tweet|
+  client.update("@#{tweet.user.screen_name} really ruby is alive!! https://isrubydead.com/ ")
+end
+
+# my topics to serach
+topics = ['#ruby', '#hire', '#remote']
+stream.filter(track: topics.join(',')) do |tweet|
+  if tweet.is_a?(Twitter::Tweet)
+    puts tweet.text
+    # r_client.fav tweet
+  end
+end
+
+
+tweets = client.user_timeline('@RubygemsN', count: 8, since_id: 1319379892555845632) #max_id
+tweets.each { |tweet| puts tweet.full_text }
+
