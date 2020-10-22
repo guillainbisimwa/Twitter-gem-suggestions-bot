3b8a90292e92d74ec4492b07cc6687d765196db2
guillainbisimwa <guillainbisimwa@hotmail.fr>
2020-10-23 00:48:14 +0200

    Create an object

--- lib/config.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-# frozen_string_literal: true
-
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key = ''
-  config.consumer_secret = ''
-  config.access_token = ''
-  config.access_token_secret = ''
-end
