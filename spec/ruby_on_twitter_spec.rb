d395fdd7d9ec5dad8193fdfde88e052c5f24c560
guillainbisimwa <guillainbisimwa@hotmail.fr>
2020-10-25 23:19:33 +0200

    Format a tweet method

---
+++ spec/ruby_on_twitter_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require './lib/ruby_on_twitter'
+
+describe 'Format a tweet method' do
+  let(:ruby_twitter) { RubyOnTwitter.new }
+
+  it 'Should not return nil' do
+    expect(ruby_twitter.format_text('')).not_to be_nil
+  end
+end
+
+describe 'Format a tweet method' do
+  let(:ruby_twitter) { RubyOnTwitter.new }
+
+  it 'Should return an array' do
+    expect(ruby_twitter.format_text('gem (0.3.1): details. https://link')).to eql(['gem', 'details.', 'https://link'])
+  end
+end
+
+describe 'Format a tweet method' do
+  let(:ruby_twitter) { RubyOnTwitter.new }
+  let(:t) { ruby_twitter.format_text('gem (0.3.1): details. https://link')}
+  it 'Should return an array of length 3' do
+    expect(t.length).to eql(3)
+  end
+end
