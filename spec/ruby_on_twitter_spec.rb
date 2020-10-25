de238d6980bf47fc3a13febddef62d569b5efda5
guillainbisimwa <guillainbisimwa@hotmail.fr>
2020-10-26 00:05:55 +0200

    Fix linters failures

--- spec/ruby_on_twitter_spec.rb
+++ spec/ruby_on_twitter_spec.rb
@@ -1,3 +1,4 @@
+require 'dotenv'
 require './lib/ruby_on_twitter'
 
 describe 'Format a tweet method' do
@@ -18,7 +19,7 @@ end
 
 describe 'Format a tweet method' do
   let(:ruby_twitter) { RubyOnTwitter.new }
-  let(:t) { ruby_twitter.format_text('gem (0.3.1): details. https://link')}
+  let(:t) { ruby_twitter.format_text('gem (0.3.1): details. https://link') }
   it 'Should return an array of length 3' do
     expect(t.length).to eql(3)
   end
